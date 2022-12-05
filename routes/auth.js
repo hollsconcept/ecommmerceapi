535520fa9d3f4714fdf18460b93104fc9bb0c70b
unknown <you@example.com>
2022-12-05 11:35:33 +0100

    added auth files

--- routes/auth.js
+++ routes/auth.js
@@ -30,12 +30,13 @@ router.post("/login" , async function(req,res){
         OriginalPassword !==req.body.password && res.status(401).json("wrong username or password");
         const accessToken = jwt.sign({
             id:user._id,
-            isAdmin:user.isAdmin,
-        },process.env.JWT_SEC,{expiresIn:"3d"});
+            isAdmin:user.isAdmin},
+            process.env.JWT_SEC,
+            {expiresIn:"3d"});
         const{password,...others}= user._doc;
        
       
-        res.status(200).json({...others,accessToken});
+        res.status(200).json({others,accessToken});
     }catch(err){
         res.status(500).json(err);
     }
